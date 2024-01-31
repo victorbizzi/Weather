33cff1aa9a8ce79b6fed2f5828363f6f7f370513
victorabgdm <136820074+victorabgdm@users.noreply.github.com>
2024-01-31 14:44:36 +0000

    COMMIT

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import datetime as dt
 import requests
-from randomCity import get_random_capital_city
 from randomCity import capital_city
 
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 API_KEY = open('api_key.txt', 'r').read()
 CITY = capital_city
 
+
 def kelvinToCelsiusFahrenheit(kelvin):
     celsius = kelvin - 273.15
     fahrenheit = celsius * (9/5) + 32
     return celsius, fahrenheit
+
 
 url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
 
@@ -25,5 +26,4 @@ description = response['weather'][0]['description']
 sunrise_time = dt.datetime.utcfromtimestamp(response['sys']['sunrise'] + response['timezone'])
 sunset_time = dt.datetime.utcfromtimestamp(response['sys']['sunrise'] + response['timezone'])
 
-#print(response)
 print(f"Temperature in {CITY}: {temp_celsius:.2f}")
